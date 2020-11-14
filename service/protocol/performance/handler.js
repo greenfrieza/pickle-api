2f688b83faaaf291f6b97cf73c1409a825c40b73
Cristian Vallejo <cdv1013@gmail.com>
2020-11-13 21:44:32 -0500

    use more accurate scaling, update api paths

--- service/protocol/performance/handler.js
+++ service/protocol/performance/handler.js
@@ -6,20 +6,41 @@ const ONE_DAY = 48;
 const THREE_DAYS = ONE_DAY * 3;
 const SEVEN_DAYS = ONE_DAY * 7;
 const THIRTY_DAYS = ONE_DAY * 30;
+const SAMPLE_DAYS = THIRTY_DAYS + 1;
 const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
 
 // helper functions
-const getRatio = (data, offset) => data.length > offset ? data[data.length - (offset + 1)].ratio : undefined;
-const getTimestamp = (data, offset) => data.length > offset ? data[data.length - (offset + 1)].timestamp : undefined;
-const getPerformance = (cRatio, pRatio, cTime, pTime) => {
-  if (pRatio) {
-    const timeDiff = cTime - pTime;
-    const scalar = (365 / days) * blocks;
-    const slope = (cRatio - pRatio) / blocks;
-    return scalar * slope * 100;
-  }
-  return undefined;
+const getRatio = (data, offset) => data.length >= offset ? data[data.length - (offset + 1)].ratio : undefined;
+const getBlock = (data, offset) => data.length >= offset ? data[data.length - (offset + 1)].height : undefined;
+const getTimestamp = (data, offset) => data.length >= offset ? data[data.length - (offset + 1)].timestamp : undefined;
+
+const getPerformance = (ratioDiff, blockDiff, timeDiff) => {
+  const scalar = (ONE_YEAR_MS / timeDiff) * blockDiff;
+  const slope = ratioDiff / blockDiff;
+  return scalar * slope * 100;
 };
+
+const getSamplePerformance = (data, offset) => {
+  // get current values
+  const currentRatio = getRatio(data, CURRENT);
+  const currentBlock = getBlock(data, CURRENT);
+  const currentTimestamp = getTimestamp(data, CURRENT);
+
+  // get sampled ratios
+  const sampledRatio = getRatio(data, offset);
+  const sampledBlock = getBlock(data, offset);
+  const sampledTimestamp = getTimestamp(data, offset);
+
+  if (!sampledRatio || !sampledBlock || !sampledTimestamp) {
+    return undefined;
+  }
+
+  const ratioDiff = currentRatio - sampledRatio;
+  const blockDiff = currentBlock - sampledBlock;
+  const timestampDiff = currentTimestamp - sampledTimestamp;
+  return getPerformance(ratioDiff, blockDiff, timestampDiff);
+};
+
 
 exports.handler = async (event) => {
   if (event.source === "serverless-plugin-warmup") {
@@ -27,24 +48,11 @@ exports.handler = async (event) => {
   }
 
   const asset = event.pathParameters.jarname;
-  const data = await getAssetData(process.env.ASSET_DATA, asset, THIRTY_DAYS);
-
-  // get ratios
-  const currentRatio = getRatio(data, CURRENT);
-  const threeDayRatio = getRatio(data, THREE_DAYS);
-  const sevenDayRatio = getRatio(data, SEVEN_DAYS);
-  const thirtyDayRatio = getRatio(data, THIRTY_DAYS);
-
-  // get blocks
-  const currentBlock = getTimestamp(data, CURRENT);
-  const threeDayBlock = getTimestamp(data, THREE_DAYS);
-  const sevenDayBlock = getTimestamp(data, SEVEN_DAYS);
-  const thirtyDayBlock = getTimestamp(data, THIRTY_DAYS);
-
+  const data = await getAssetData(process.env.ASSET_DATA, asset, SAMPLE_DAYS);
   const performanceData = {
-    threeDay: getPerformance(currentRatio, threeDayRatio, currentBlock, threeDayBlock),
-    sevenDay: getPerformance(currentRatio, sevenDayRatio, currentBlock, sevenDayBlock),
-    thirtyDay: getPerformance(currentRatio, thirtyDayRatio, currentBlock, thirtyDayBlock),
+    threeDay: getSamplePerformance(data, THREE_DAYS),
+    sevenDay: getSamplePerformance(data, SEVEN_DAYS),
+    thirtyDay: getSamplePerformance(data, THIRTY_DAYS),
   };
 
   return {
